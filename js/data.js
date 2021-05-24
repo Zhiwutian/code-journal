dcaae85bab6f81d4a483ff89b5ac2351b29a827c
Brett Albright <balbright@learningfuze.com>
2021-05-24 16:29:06 -0700

    completed user can create entry

--- js/data.js
+++ js/data.js
@@ -6,3 +6,12 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var storedData = JSON.parse(localStorage.getItem('entry-data'));
+if (storedData !== null) {
+  data = storedData;
+}
+window.addEventListener('beforeunload', function (event) {
+  var currentData = JSON.stringify(data);
+  localStorage.setItem('entry-data', currentData);
+});

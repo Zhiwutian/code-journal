9b7a9eb568227da264d7e344ca7b20dce03c6127
Brett Albright <balbright@learningfuze.com>
2021-05-24 12:47:36 -0700

    completed entry photo update on input event

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 /* global data */
 /* exported data */
+
+var $photoInput = document.getElementById("photoUrl");
+var $entryImage = document.getElementById("formImage");
+
+$photoInput.addEventListener("input", function(event) {
+  $entryImage.setAttribute("src", event.target.value);
+});

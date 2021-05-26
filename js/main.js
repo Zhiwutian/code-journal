9a7def35702c52fb0267ebaef451bbb1452c639a
Brett Albright <balbright@learningfuze.com>
2021-05-25 20:55:12 -0700

    completed entry rendering functionality , including check for no entries

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 var $photoInput = document.getElementById('photoUrl');
 var $entryImage = document.getElementById('formImage');
 var $entryForm = document.getElementById('entryForm');
-
+var $entriesUl = document.getElementById('entries-list');
 $photoInput.addEventListener('input', function (event) {
   $entryImage.setAttribute('src', event.target.value);
 });
@@ -22,3 +22,61 @@ $entryForm.addEventListener('submit', function (event) {
   $entryImage.setAttribute('src', './images/placeholder-image-square.jpg');
   $entryForm.reset();
 });
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  if (!data.entries.length) {
+    $entriesUl.append(renderNoEntries());
+  } else {
+    for (var entryIndex = 0; entryIndex < data.entries.length; entryIndex++) {
+      var renderedEntry = renderEntry(data.entries[entryIndex]);
+      $entriesUl.append(renderedEntry);
+    }
+  }
+
+});
+
+function renderNoEntries() {
+  var $li = document.createElement('li');
+
+  var $textContainer = document.createElement('div');
+  $textContainer.className = 'row column-full justify-center';
+
+  var $textP = document.createElement('p');
+  $textP.textContent = 'No entries have been recorded';
+
+  $textContainer.append($textP);
+  $li.append($textContainer);
+
+  return $li;
+}
+
+function renderEntry(entryInfo) {
+
+  var $li = document.createElement('li');
+
+  var $entryRow = document.createElement('div');
+  $entryRow.className = 'column-full row margin-spacer';
+
+  var $imageColumn = document.createElement('div');
+  $imageColumn.className = 'column-half margin-spacer';
+
+  var $entryImage = document.createElement('img');
+  $entryImage.className = 'column-full';
+  $entryImage.src = entryInfo.photoUrl;
+
+  var $textColumn = document.createElement('div');
+  $textColumn.className = 'column-half margin-spacer';
+
+  var $entryTitle = document.createElement('h2');
+  $entryTitle.textContent = entryInfo.title;
+
+  var $entryNotes = document.createElement('p');
+  $entryNotes.textContent = entryInfo.notes;
+
+  $imageColumn.append($entryImage);
+  $textColumn.append($entryTitle, $entryNotes);
+  $entryRow.append($imageColumn, $textColumn);
+  $li.append($entryRow);
+
+  return $li;
+}
